53709e27a93e299850e2d3e988b81e9ded5b62b3
Tim McNamara <code@timmcnamara.co.nz>
2012-08-11 14:59:20 +1200

    setup.sh, now with more Python

--- setup.sh
+++ setup.sh
@@ -6,11 +6,12 @@ sudo apt-get -y upgrade
 echo 'unsuckifying system by installing basic libs that should have been there in the first place'
 sudo apt-get install -y build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libxml2-dev libxslt-dev
 echo 'done'
-echo 'installing ruby and rubygems'
-sudo apt-get -y -t universe install ruby rubygems
+echo 'installing python and pip'
+sudo apt-get -y -t universe install python python-setuptools
+sudo easy_install pip
 echo 'done'
-echo 'installing AWS Ruby SDK which should also have been there in the first place'
-sudo gem install aws-sdk --source http://rubygems.org
+echo 'installing boto which should also have been there in the first place'
+sudo pip install boto
 echo 'done'
 echo 'OK, good to go'
 exit 0
